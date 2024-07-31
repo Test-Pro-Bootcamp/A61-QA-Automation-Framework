e0ffe600737395892e0c2acb658e8571f9e48698
barrau89 <barrau89@gmail.com>
2024-07-30 23:50:27 -0500

    adding loginWithEmailAndPassword test in LoginTest class

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -13,6 +15,31 @@ public class LoginTests extends BaseTest {
         url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
+
+    @Test
+    public void loginWithEmailAndPassword() {
+
+        // Step 1
+        url = "https://qa.koel.app/";
+        driver.get(url);
+
+        // Step 2
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("barrau89@gmail.com");
+
+        // Step 3
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent");
+
+        // Step 4
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+
+
+
 
     }
 }
