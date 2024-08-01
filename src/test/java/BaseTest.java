c7ebc02e3a43b54c2a4745f8c13e8b2da241c2e5
barrau89 <barrau89@gmail.com>
2024-08-01 00:19:54 -0500

    Wait added to BaseTest class and hoverOverMediaPlayer method was added to Homework18

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -14,6 +15,7 @@ public class BaseTest {
 
     public WebDriver driver;
     public String url;
+    public WebDriverWait wait;
 
     @BeforeSuite
     static void setupClass() {
@@ -28,6 +30,8 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @AfterMethod
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,14 +17,25 @@ public class Homework18 extends BaseTest {
         inputPassword("te$t$tudent");
         clickLoginButton();
 
+        //Hover over web media
+        hoverOverMediaPlayer();
+
         //Play a song
         clickMediaPlayButton();
+
+        //Validate song is playing
         validateSongIsPlaying();
 
     }
 
+    private void hoverOverMediaPlayer() {
+        WebElement mediaElement = driver.findElement(By.cssSelector("span[class='play']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(mediaElement).perform();
+    }
+
     private void clickMediaPlayButton() {
-        WebElement playButton = driver.findElement(By.cssSelector("span[class='play']"));
+        WebElement playButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class='play']")));
         playButton.click();
     }
 
