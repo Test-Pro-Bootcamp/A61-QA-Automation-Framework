296c4eacd0ec49374b2fcb74e0b5ac817472f1cd
praveenag1 <praveena.gudlanarva@testpro.io>
2024-09-04 19:29:54 -0400

    Homework_18
    while playing songs, checking the sound bars are visible or not

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,63 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.Current;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class BaseTest {
 
-    @BeforeSuite
-    static void setupClass() {
+    public WebDriver driver;
+    /* public String url="https://qa.koel.app/"; */
+   /* @DataProvider(name="LoginNegativeTestData")
+        public Object[][] getDataFromDataProvider(){
+        return new Object[][]  {
+                {"invalidEmail@gmail.com", "invalid"},
+                {"demo@gmail.com", "     "},
+                {"        ", "   "},
+                {"          ", " Veena@123  "},
+        };
+        }*/
+        @BeforeSuite
+        static void setupClass() {
         WebDriverManager.chromedriver().setup();
-    }
-}
+      }
+      @BeforeMethod
+     @Parameters({"BaseURL"})
+      public void launchBrowser(String baseURL){
+        // add chromeArguments
+        String url= baseURL;
+        ChromeOptions options =new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        navigateToPage(url);
+      }
+      @AfterMethod
+      public void closeBrowser(){
+        driver.quit();
+      }
+      public void navigateToPage(String url){
+        driver.get(url);
+      }
+      public void provideEmail(String email){
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+       }
+      public void providePassword(String password){
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+        }
+        public void loginBtn(){
+        WebElement loginButton= driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+
+      }
+      }
---
+++ src/test/java/PlaySongs.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PlaySongs extends BaseTest{
+    @Test
+    public void PlaySongTest()throws InterruptedException{
+        provideEmail("Shuban.laddu@gmail.com");
+        providePassword("Pavani@10");
+        loginBtn();
+        Thread.sleep(2000);
+        clickPlay();
+        Assert.assertTrue(isSongPlaying());
+
+    }
+    public void clickPlay() throws InterruptedException {
+        WebElement playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        playNextButton.click();
+        WebElement playBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        playBtn.click();
+        Thread.sleep(3000);
+    }
+    public boolean isSongPlaying(){
+        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+        return soundBar.isDisplayed();
+    }
+
+
+
+
+}
