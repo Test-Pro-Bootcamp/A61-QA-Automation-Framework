9740ecb5b1f45a1a0a94b1e39925972b36138965
praveenag1 <praveena.gudlanarva@testpro.io>
2024-09-05 19:19:19 -0400

    Homework19
    deleting the selected Playlist from the Playlist list and getting the notification on the APP as our Playlist got deleted

---
+++ src/test/java/DeletePlaylist.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeletePlaylist extends BaseTest{
+    @Test
+    public void DeletePlaylistTest()throws InterruptedException{
+        String expectedPlaylistDeletedMsg = "Deleted playlist \"pav.\"";
+
+        provideEmail("Shuban.laddu@gmail.com");
+        providePassword("Pavani@10");
+        loginBtn();
+        Thread.sleep(3000);
+        OpenSelectedPlaylist();
+        clickDeletePlaylistBtn();
+        byte[] expectedPlaylistDeletedMessage;
+        Assert.assertEquals(getDeletedPlaylistMsg(),expectedPlaylistDeletedMsg);
+    }
+    public void OpenSelectedPlaylist() throws InterruptedException {
+        WebElement PlaylistName = driver.findElement(By.cssSelector("li.playlist:nth-child(3)"));
+        PlaylistName.click();
+        Thread.sleep(2000);
+    }
+    public void clickDeletePlaylistBtn() throws InterruptedException {
+        WebElement DeletePlaylistBtn = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        DeletePlaylistBtn.click();
+        Thread.sleep(2000);
+    }
+    public String getDeletedPlaylistMsg(){
+        WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationMsg.getText();
+    }
+}
